5e47eaf1d60730db3deac5d69f367823163d2ddc
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-06-05 12:11:15 +0100

    EDITED: extension webview html alignment and added basic setup to readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # vscode-rapid-gui
+
+## Setup
+
+1. Install required packages with:
+
+```bash
+npm install
+```
+
+2. Run VScode extension debugger (F5).
+
+3. In VScode command palette run ">Open drag and drop editor".
--- src/webview.jsx
+++ src/webview.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Typography, Paper, Grid, makeStyles } from '@material-ui/core';
-import { Editor, Frame, Element } from '@craftjs/core';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Typography, Paper, Grid, makeStyles } from "@material-ui/core";
+import { Editor, Frame, Element } from "@craftjs/core";
 
-import { SettingsPanel } from './components/SettingsPanel';
-import { Toolbox } from './components/Toolbox';
-import { Topbar } from './components/Topbar';
-import { Button } from './components/user/Button';
-import { Card, CardBottom, CardTop } from './components/user/Card';
-import { Container } from './components/user/Container';
-import { Text } from './components/user/Text';
+import { SettingsPanel } from "./components/SettingsPanel";
+import { Toolbox } from "./components/Toolbox";
+import { Topbar } from "./components/Topbar";
+import { Button } from "./components/user/Button";
+import { Card, CardBottom, CardTop } from "./components/user/Card";
+import { Container } from "./components/user/Container";
+import { Text } from "./components/user/Text";
 
 const useStyles = makeStyles(() => ({
   root: {
     padding: 0,
-    background: 'rgb(252, 253, 253)',
+    background: "rgb(252, 253, 253)",
+  },
+  fullHeight: {
+    height: "100%",
+  },
+  gridItem: {
+    display: "flex",
+    flexDirection: "column",
   },
 }));
 
@@ -32,40 +39,48 @@ const App = () => {
         CardBottom,
       }}
     >
-      <Topbar />
-      <Grid container spacing={5} style={{ paddingTop: '10px' }}>
-        <Grid item xs>
-          <Frame>
-            <Element
-              canvas
-              is={Container}
-              padding={5}
-              background="#eeeeee"
-              data-cy="root-container"
-            >
-              <Card data-cy="frame-card" />
-              <Button text="Click me" size="small" data-cy="frame-button" />
-              <Text fontSize={20} text="Hi world!" data-cy="frame-text" />
+      <Grid
+        container
+        spacing={5}
+        style={{ paddingTop: "10px", height: "100vh" }}
+        alignItems="flex-start"
+        className={classes.fullHeight}
+      >
+        <Grid item xs={8} className={classes.gridItem}>
+          <Paper className={classes.root} style={{ flex: 1 }}>
+            <SettingsPanel />
+            <Topbar />
+            <Frame>
               <Element
                 canvas
                 is={Container}
-                padding={6}
-                background="#999999"
-                data-cy="frame-container"
+                padding={5}
+                background="#eeeeee"
+                data-cy="root-container"
               >
-                <Text
-                  size="small"
-                  text="It's me again!"
-                  data-cy="frame-container-text"
-                />
+                <Card data-cy="frame-card" />
+                <Button text="Click me" size="small" data-cy="frame-button" />
+                <Text fontSize={20} text="Hi world!" data-cy="frame-text" />
+                <Element
+                  canvas
+                  is={Container}
+                  padding={6}
+                  background="#999999"
+                  data-cy="frame-container"
+                >
+                  <Text
+                    size="small"
+                    text="It's me again!"
+                    data-cy="frame-container-text"
+                  />
+                </Element>
               </Element>
-            </Element>
-          </Frame>
+            </Frame>
+          </Paper>
         </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.root}>
+        <Grid item xs={4} className={classes.gridItem}>
+          <Paper className={classes.root} style={{ flex: 1 }}>
             <Toolbox />
-            <SettingsPanel />
           </Paper>
         </Grid>
       </Grid>
@@ -73,4 +88,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('editor'));
+ReactDOM.render(<App />, document.getElementById("editor"));
