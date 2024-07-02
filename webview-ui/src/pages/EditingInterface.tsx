65631185c54fa40a694028dbb2a3fa2be727979b
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-07-02 12:54:05 +0100

    editing interface

---
+++ webview-ui/src/pages/EditingInterface.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditingInterface: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Editing Interface!</h1>
+            <p>This is a simple page.</p>
+        </div>
+    );
+};
+
+export default EditingInterface;
