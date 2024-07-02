687b8b9ee08774bbd06e701ee20b72ca78f03ea4
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-07-02 14:15:39 +0100

    EDITED: navbar tests

--- webview-ui/src/components/NavBar/NavBar.tsx
+++ webview-ui/src/components/NavBar/NavBar.tsx
@@ -1,45 +1,73 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { CursorRegular, SlideMultipleRegular, DocumentFolderRegular, DeleteRegular, AddFilled } from "@fluentui/react-icons";
-import { Button } from '@fluentui/react-components';
-import './NavBar.css';
+import {
+  NavDrawer,
+  NavDrawerHeader,
+  NavDrawerBody,
+  NavItem,
+  NavDivider,
+  Hamburger,
+} from '@fluentui/react-nav-preview';
+import {  Tooltip,
+  makeStyles,}
+  from '@fluentui/react-components'
+import {
+  Board20Filled,
+  Board20Regular,
+  DocumentBulletListMultiple20Filled,
+  DocumentBulletListMultiple20Regular,
+  BoxMultiple20Filled,
+  BoxMultiple20Regular,
+  Delete20Filled,
+  Delete20Regular,
+  bundleIcon,
+} from '@fluentui/react-icons';
+
+const useStyles = makeStyles({
+  root: {
+    overflow: 'hidden',
+    display: 'flex',
+    height: '100vh',
+  },
+});
+
+const Dashboard = bundleIcon(Board20Filled, Board20Regular);
+const Templates = bundleIcon(DocumentBulletListMultiple20Filled, DocumentBulletListMultiple20Regular);
+const Projects = bundleIcon(BoxMultiple20Filled, BoxMultiple20Regular);
+const Deleted = bundleIcon(Delete20Filled, Delete20Regular);
 
 const NavBar: React.FC = () => {
-  return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/my-projects">
-            <Button appearance="primary" icon={<AddFilled />}>New Project</Button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <CursorRegular className="icon" />
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/templates">
-            <SlideMultipleRegular className="icon" />
-            Templates
-          </Link>
-        </li>
-        <li>
-          <Link to="/my-projects">
-            <DocumentFolderRegular className="icon" />
-            My Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="/deleted">
-            <DeleteRegular className="icon" />
-            Deleted
-          </Link>
-        </li>
-      </ul>
-    </nav>
+  const styles = useStyles();
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  const renderHamburgerWithToolTip = () => (
+    <Tooltip content="Navigation" relationship="label">
+      <Hamburger onClick={() => setIsOpen(!isOpen)} />
+    </Tooltip>
   );
-}
+
+  return (
+    <div className={styles.root}>
+      <NavDrawer open={isOpen} type="inline">
+        <NavDrawerHeader>{renderHamburgerWithToolTip()}</NavDrawerHeader>
+        <NavDrawerBody>
+          <NavItem href="/" icon={<Dashboard />} value="1">
+            Home
+          </NavItem>
+          <NavItem href="/templates" icon={<Templates />} value="2">
+            Templates
+          </NavItem>
+          <NavItem href="/my-projects" icon={<Projects />} value="3">
+            My Projects
+          </NavItem>
+          <NavItem href="/deleted" icon={<Deleted />} value="4">
+            Deleted
+          </NavItem>
+          <NavDivider />
+        </NavDrawerBody>
+      </NavDrawer>
+      {!isOpen && renderHamburgerWithToolTip()}
+    </div>
+  );
+};
 
 export default NavBar;
--- webview-ui/src/layouts/MainLayout.tsx
+++ webview-ui/src/layouts/MainLayout.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar/NavBar';
-import './MainLayout.css';
+import { makeStyles, shorthands } from '@fluentui/react-components';
+
+const useStyles = makeStyles({
+  mainLayout: {
+    display: 'flex',
+    height: '100vh',
+  },
+  content: {
+    flexGrow: 1, /* Take up remaining space */
+    ...shorthands.padding('20px'),
+    overflowY: 'auto',
+    boxSizing: 'border-box', /* Include padding in width and height calculations */
+    backgroundColor: '#2a2a2a', /* Background color of the content area to match the body */
+    color: '#fff', /* Text color */
+  },
+});
 
 const MainLayout: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="main-layout">
+    <div className={classes.mainLayout}>
       <NavBar />
-      <main className="content">
+      <main className={classes.content}>
         <Outlet />
       </main>
     </div>
