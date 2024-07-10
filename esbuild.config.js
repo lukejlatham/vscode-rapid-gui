00ad723541072ed1331780c5942ff5d4462fef07
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-07-10 14:12:28 +0100

    REMOVED: old sketch processing files

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,21 +1,10 @@
 const esbuild = require('esbuild');
-const dotenv = require('dotenv');
 const path = require('path');
-
-// Load environment variables from .env file in the root directory
-dotenv.config({ path: path.join(__dirname, '.env') });
-
-// Create an object to define environment variables for esbuild
-const defineEnv = Object.keys(process.env).reduce((acc, key) => {
-  acc[`process.env.${key}`] = JSON.stringify(process.env[key]);
-  return acc;
-}, {});
 
 esbuild.build({
   entryPoints: [path.join(__dirname, 'webview-ui/src/index.tsx')],
   bundle: true,
   outfile: path.join(__dirname, 'out', 'panels', 'MainWebviewPanel.js'),  // Place output in the expected directory
-  define: defineEnv,
   platform: 'browser',
   target: 'esnext',
   loader: {
