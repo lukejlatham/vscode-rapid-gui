ecdee4a5aa4125ba3bff8c136a42aca86c5febfb
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-07-11 11:29:46 +0100

    ADDED: base azure key storage commands

--- src/utilities/azureApiKeyStorage.ts
+++ src/utilities/azureApiKeyStorage.ts
@@ -19,8 +19,16 @@ export function activate(context: vscode.ExtensionContext) {
         await secretStorage.store("AZURE_OPENAI_API_KEY", apiKey);
         await secretStorage.store("AZURE_OPENAI_API_ENDPOINT", apiEndpoint);
         await secretStorage.store("GPT4O_DEPLOYMENT_NAME", deploymentName);
-        vscode.window.showInformationMessage("Added API keys to storage");
+        vscode.window.showInformationMessage("Added API keys to storage.");
       }
     })
   );
+}
+
+async function getAzureOpenaiApiKeys(context: vscode.ExtensionContext) {
+  const apiKey = await context.secrets.get("AZURE_OPENAI_API_KEY");
+  const apiEndpoint = await context.secrets.get("AZURE_OPENAI_API_ENDPOINT");
+  const deploymentName = await context.secrets.get("GPT4O_DEPLOYMENT_NAME");
+
+  return { apiKey, apiEndpoint, deploymentName };
 }
