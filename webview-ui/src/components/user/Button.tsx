19b84da993a4217a558f072714d0edd345fefd58
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-07-11 18:13:45 +0100

    Added more customisations to buttons

--- webview-ui/src/components/user/Button.tsx
+++ webview-ui/src/components/user/Button.tsx
@@ -5,11 +5,14 @@ import { Input, Label } from "@fluentui/react-components";
 interface ButtonProps {
     backgroundColor: string;
     fontSize: number;
+    fontColor: string;
     borderRadius: number;
+    width: number;
+    height: number;
     text: string;
 }
 
-export const Button: UserComponent<ButtonProps> = ({ backgroundColor, fontSize, borderRadius, text }) => {
+export const Button: UserComponent<ButtonProps> = ({ backgroundColor, fontSize, borderRadius, text, fontColor, width, height }) => {
     const { connectors: { connect, drag } } = useNode();
 
     return (
@@ -17,7 +20,7 @@ export const Button: UserComponent<ButtonProps> = ({ backgroundColor, fontSize, 
             if (ref) {
                 connect(drag(ref));
             }
-        }} style={{ padding: "10px", color: 'white', border: 'none', backgroundColor, fontSize: `${fontSize}px`, borderRadius: `${borderRadius}px` }}>
+        }} style={{ padding: "10px", color: fontColor, border: 'none', backgroundColor, fontSize: `${fontSize}px`, borderRadius: `${borderRadius}px`, width: `${width}px`, height: `${height}px` }}>
             {text}
         </button>
     );
@@ -29,11 +32,19 @@ const ButtonSettings: React.FC = () => {
     }));
 
     return (
-        <div style={{display: 'flex', flexDirection: 'column', gap: '10px', padding: '5px'}}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', padding: '5px' }}>
             <Label>
-                Color
+                Font Color
                 <input
-                    style={{ width: "100%", borderRadius: "4px", height: "35px"}}
+                    style={{ width: "100%", borderRadius: "4px", height: "35px" }}
+                    type="color"
+                    defaultValue={props.fontColor}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setProp((props: ButtonProps) => props.fontColor = e.target.value)} />
+            </Label>
+            <Label>
+                Background Color
+                <input
+                    style={{ width: "100%", borderRadius: "4px", height: "35px" }}
                     type="color"
                     defaultValue={props.backgroundColor}
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => setProp((props: ButtonProps) => props.backgroundColor = e.target.value)} />
@@ -49,25 +60,51 @@ const ButtonSettings: React.FC = () => {
                 />
             </Label>
             <Label>
-            Border Radius
-            <Input
-                type="number"
-                min={0}
-                defaultValue={props.borderRadius.toString()} // Convert the number to a string
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setProp((props: ButtonProps) => (props.borderRadius = parseInt(e.target.value, 10)), 1000);
-                }}
-            />
+                Border Radius
+                <Input
+                    type="number"
+                    min={0}
+                    defaultValue={props.borderRadius.toString()} // Convert the number to a string
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setProp((props: ButtonProps) => (props.borderRadius = parseInt(e.target.value, 10)), 1000);
+                    }}
+                />
             </Label>
             <Label>
-            Text
-            <Input
-                type="text"
-                defaultValue={props.text}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setProp((props: ButtonProps) => (props.text = e.target.value), 1000);
-                }}
-            />
+                Width
+                <Input
+                    style={{ width: "100%"}}
+                    type="number"
+                    min={0}
+                    max={900}
+                    defaultValue={props.width.toString()} // Convert the number to a string
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setProp((props: ButtonProps) => (props.width = parseInt(e.target.value, 10)), 1000);
+                    }}
+                />
+                </Label>
+            <Label>
+                Height
+                <Input
+                    style={{ width: "100%"}}
+                    type="number"
+                    min={0}
+                    max={800}
+                    defaultValue={props.height.toString()} // Convert the number to a string
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setProp((props: ButtonProps) => (props.height = parseInt(e.target.value, 10)), 1000);
+                    }}
+                />
+            </Label>
+            <Label>
+                Text
+                <Input
+                    type="text"
+                    defaultValue={props.text}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setProp((props: ButtonProps) => (props.text = e.target.value), 1000);
+                    }}
+                />
             </Label>
         </div>
     );
@@ -75,9 +112,12 @@ const ButtonSettings: React.FC = () => {
 
 export const ButtonDefaultProps: ButtonProps = {
     backgroundColor: "#0047AB",
+    fontColor: "white",
     fontSize: 20,
     borderRadius: 4,
-    text: "New Button"
+    text: "New Button",
+    width: 150,
+    height: 80
 };
 
 Button.craft = {
