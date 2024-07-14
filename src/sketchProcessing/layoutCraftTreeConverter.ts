bde76357fcbc0a0eacd3bdcd785650ed441878c4
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-07-14 17:21:16 +0100

    ADDED: default props to node processing

--- src/sketchProcessing/layoutCraftTreeConverter.ts
+++ src/sketchProcessing/layoutCraftTreeConverter.ts
@@ -1,3 +1,43 @@
+// Define the default props
+const TextboxDefaultProps = {
+  text: "",
+  fontSize: 16,
+  fontColor: "black",
+  backgroundColor: "white",
+  placeholder: "Placeholder...",
+  rows: 5,
+  cols: 20,
+  borderRadius: 5,
+  alignment: "centre",
+};
+
+const LabelDefaultProps = {
+  text: "New Label",
+  textAlign: "left",
+  fontSize: 20,
+  color: "#FFFFF",
+  userEditable: true,
+};
+
+const ButtonDefaultProps = {
+  backgroundColor: "#0047AB",
+  fontColor: "white",
+  fontSize: 20,
+  borderRadius: 4,
+  text: "New Button",
+  width: 150,
+  height: 50,
+  alignment: "centre",
+};
+
+const ImageDefaultProps = {
+  src: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+  alt: "New image",
+  width: 480,
+  height: 320,
+  alignment: "center",
+};
+
 type NodeType =
   | "Background"
   | "Container"
@@ -48,6 +88,14 @@ export async function convertToFullVersion(simplifiedNodes: SimplifiedNode[]): P
       props = { numberOfCols: Object.keys(node.linked_nodes).length, gap: 0 };
     } else if (node.type === "Rows" && node.numberOfRows !== undefined) {
       props = { numberOfRows: node.numberOfRows, gap: 0 };
+    } else if (node.type === "Textbox") {
+      props = TextboxDefaultProps;
+    } else if (node.type === "Label") {
+      props = LabelDefaultProps;
+    } else if (node.type === "Button") {
+      props = ButtonDefaultProps;
+    } else if (node.type === "Image") {
+      props = ImageDefaultProps;
     }
 
     const fullNode: FullNode = {
