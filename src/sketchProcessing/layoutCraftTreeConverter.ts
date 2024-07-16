df47bf7278cdb3940e1de5ea0373985a2397dba7
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-07-16 11:02:08 +0100

    ADDED: testing different states

--- src/sketchProcessing/layoutCraftTreeConverter.ts
+++ src/sketchProcessing/layoutCraftTreeConverter.ts
@@ -105,7 +105,7 @@ export async function convertToFullVersion(simplifiedNodes: SimplifiedNode[]): P
       displayName: node.type,
       custom: {},
       hidden: false,
-      nodes: node.nodes,
+      nodes: node.nodes || [],
       linkedNodes: node.linked_nodes || {},
       ...(parentId && { parent: parentId }),
     };
@@ -127,73 +127,3 @@ export async function convertToFullVersion(simplifiedNodes: SimplifiedNode[]): P
 
   return fullNodes;
 }
-
-// Example usage
-const simplifiedNodes: SimplifiedNode[] = [
-  { id: "ROOT", type: "Background", nodes: ["ContainerID"] },
-  { id: "ContainerID", type: "Container", nodes: ["Rows1"] },
-  {
-    id: "Rows1",
-    type: "Rows",
-    numberOfRows: 3,
-    nodes: [],
-    linked_nodes: { "row-0": "Row1", "row-1": "Row2", "row-2": "Row3" },
-  },
-  { id: "Row1", type: "Row", nodes: ["Columns1"] },
-  {
-    id: "Columns1",
-    type: "Columns",
-    nodes: [],
-    linked_nodes: {
-      "column-0": "Column1",
-      "column-1": "Column2",
-      "column-2": "Column3",
-      "column-3": "Column4",
-      "column-4": "Column5",
-    },
-  },
-  { id: "Column1", type: "Column", nodes: ["Button1"] },
-  { id: "Button1", type: "Button", nodes: [] },
-  { id: "Column2", type: "Column", nodes: ["Button2"] },
-  { id: "Button2", type: "Button", nodes: [] },
-  { id: "Column3", type: "Column", nodes: ["Button3"] },
-  { id: "Button3", type: "Button", nodes: [] },
-  { id: "Column4", type: "Column", nodes: ["Button4"] },
-  { id: "Button4", type: "Button", nodes: [] },
-  { id: "Column5", type: "Column", nodes: ["Button5"] },
-  { id: "Button5", type: "Button", nodes: [] },
-  { id: "Row2", type: "Row", nodes: ["Columns2"] },
-  {
-    id: "Columns2",
-    type: "Columns",
-    nodes: [],
-    linked_nodes: { "column-0": "Column6", "column-1": "Column7" },
-  },
-  { id: "Column6", type: "Column", nodes: ["Textbox1"] },
-  { id: "Textbox1", type: "Textbox", nodes: [] },
-  { id: "Column7", type: "Column", nodes: ["Image1"] },
-  { id: "Image1", type: "Image", nodes: [] },
-  { id: "Row3", type: "Row", nodes: ["Columns3"] },
-  {
-    id: "Columns3",
-    type: "Columns",
-    nodes: [],
-    linked_nodes: {
-      "column-0": "Column8",
-      "column-1": "Column9",
-      "column-2": "Column10",
-      "column-3": "Column11",
-      "column-4": "Column12",
-    },
-  },
-  { id: "Column8", type: "Column", nodes: ["Textbox2"] },
-  { id: "Textbox2", type: "Textbox", nodes: [] },
-  { id: "Column9", type: "Column", nodes: ["Image2"] },
-  { id: "Image2", type: "Image", nodes: [] },
-  { id: "Column10", type: "Column", nodes: ["Textbox3"] },
-  { id: "Textbox3", type: "Textbox", nodes: [] },
-  { id: "Column11", type: "Column", nodes: ["Button6"] },
-  { id: "Button6", type: "Button", nodes: [] },
-  { id: "Column12", type: "Column", nodes: ["Label1"] },
-  { id: "Label1", type: "Label", nodes: [] },
-];
