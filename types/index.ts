95ecd2faf96df691aef31f600ab46f8aa4c00ec9
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-07-30 16:19:31 +0100

    EDITED: added generation interfaces to types file

--- types/index.ts
+++ types/index.ts
@@ -1,182 +1,243 @@
 //types
-import { Layout } from 'react-grid-layout';
+import { Layout } from "react-grid-layout";
 import * as VscIcons from "react-icons/vsc";
- 
+
 export interface BackgroundProps {
-    backgroundColor: string;
-    layout: Layout[];
-    rows: number;
-    columns: number;
-  }
+  backgroundColor: string;
+  layout: Layout[];
+  rows: number;
+  columns: number;
+}
 
 export interface EditBackgroundButtonProps {
-    nodeId: string;
+  nodeId: string;
 }
 
 export interface ButtonProps {
-    backgroundColor: string;
-    fontSize: number;
-    fontColor: string;
-    borderRadius: number;
-    width: number;
-    height: number;
-    text: string;
-    alignment: "left" | "center" | "right";
-    displayName?: string;
-    icon?: keyof typeof VscIcons;
-    iconPosition?: "left" | "right";
-    iconColor?: string;
-    bordercolor?: string;
-    shadow?: boolean;
-    hyperlink?: string;
+  backgroundColor: string;
+  fontSize: number;
+  fontColor: string;
+  borderRadius: number;
+  width: number;
+  height: number;
+  text: string;
+  alignment: "left" | "center" | "right";
+  displayName?: string;
+  icon?: keyof typeof VscIcons;
+  iconPosition?: "left" | "right";
+  iconColor?: string;
+  bordercolor?: string;
+  shadow?: boolean;
+  hyperlink?: string;
 }
 
+export type generateButtonProps = {
+  width: number;
+  height: number;
+  text: string;
+  icon?: keyof typeof VscIcons;
+};
+
 export type TooltipConfig = {
-    label: string;
-    content: string;
-    propKey: keyof ButtonProps;
-    type: 'color' | 'spinButton' | 'text' | 'alignment';
+  label: string;
+  content: string;
+  propKey: keyof ButtonProps;
+  type: "color" | "spinButton" | "text" | "alignment";
 };
 
 export interface CheckboxProps {
-    header: string;
-    optionLabels: string[];
-    numberOfBoxes: number;
-    fontSize: number;
-    fontColor: string;
-    direction: "row" | "column";
+  header: string;
+  optionLabels: string[];
+  numberOfBoxes: number;
+  fontSize: number;
+  fontColor: string;
+  direction: "row" | "column";
+}
+
+export interface editableCheckboxProps {
+  header: string;
+  optionLabels: string[];
+  numberOfBoxes: number;
+  direction: "row" | "column";
 }
 
 export type TooltipConfigCheckbox = {
-    label: string;
-    content: string;
-    propKey: keyof CheckboxProps;
-    type: 'color' | 'spinButton' | 'text' | 'options' | 'direction';
+  label: string;
+  content: string;
+  propKey: keyof CheckboxProps;
+  type: "color" | "spinButton" | "text" | "options" | "direction";
 };
 
 export interface ContainerProps {
-    children?: React.ReactNode;
-    flexDirection?: "row" | "column";
-    justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around";
-    alignItems?: "flex-start" | "center" | "flex-end";
-    gap?: number;
-  }
+  children?: React.ReactNode;
+  flexDirection?: "row" | "column";
+  justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around";
+  alignItems?: "flex-start" | "center" | "flex-end";
+  gap?: number;
+}
+
+export interface generateContainerProps {
+  flexDirection: "row" | "column";
+  justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around";
+  alignItems?: "flex-start" | "center" | "flex-end";
+}
 
 export interface InputProps {
-    fontSize: number;
-    fontColor: string;
-    backgroundColor: string;
-    placeholder: string;
-    borderRadius: number;
+  fontSize: number;
+  fontColor: string;
+  backgroundColor: string;
+  placeholder: string;
+  borderRadius: number;
+}
+
+export interface generateInputProps {
+  placeholder: string;
 }
 
 export type TooltipConfigInput = {
-    label: string;
-    content: string;
-    propKey: keyof InputProps;
-    type: 'color' | 'spinButton' | 'text' | 'alignment';
+  label: string;
+  content: string;
+  propKey: keyof InputProps;
+  type: "color" | "spinButton" | "text" | "alignment";
 };
 
 export interface LabelProps {
-    text: string;
-    fontSize: number;
-    fontcolor: string;
-    userEditable?: boolean;
-    width: number;
-    height: number;
-    textAlign: 'left' | 'center' | 'right' | 'justify';
-    icon?: keyof typeof VscIcons;
-    iconPosition?: 'left' | 'right';
-    iconColor?: string;
-    hyperlink?: string;
-    bold?: boolean;
-    italic?: boolean;
-    underline?: boolean;
-  }
-  
+  text: string;
+  fontSize: number;
+  fontcolor: string;
+  userEditable?: boolean;
+  width: number;
+  height: number;
+  textAlign: "left" | "center" | "right" | "justify";
+  icon?: keyof typeof VscIcons;
+  iconPosition?: "left" | "right";
+  iconColor?: string;
+  hyperlink?: string;
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+}
+
+export interface generateLabelProps {
+  text: string;
+  fontcolor: string;
+  icon?: keyof typeof VscIcons;
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+}
+
 export interface ContentEditableEvent {
-    target: { value: string };
-  }
+  target: { value: string };
+}
 
 export interface RadioButtonProps {
-    header: string;
-    numberOfButtons: number;
-    optionLabels: string[];
-    fontSize: number;
-    fontColor: string;
-    direction: "row" | "column";
+  header: string;
+  numberOfButtons: number;
+  optionLabels: string[];
+  fontSize: number;
+  fontColor: string;
+  direction: "row" | "column";
+}
+
+export interface generateRadioButtonProps {
+  header: string;
+  numberOfButtons: number;
+  optionLabels: string[];
+  direction: "row" | "column";
 }
 
 export type TooltipConfigRadio = {
-    label: string;
-    content: string;
-    propKey: keyof RadioButtonProps;
-    type: 'color' | 'spinButton' | 'text' | 'options' | 'direction';
+  label: string;
+  content: string;
+  propKey: keyof RadioButtonProps;
+  type: "color" | "spinButton" | "text" | "options" | "direction";
 };
 
 export interface TextBoxProps {
-    text: string;
-    fontSize: number;
-    fontColor: string;
-    backgroundColor: string;
-    placeholder: string;
-    borderRadius: number;
-    height: number;   //change to % after
-    width: number;   //change to % after
-    alignment: "left" | "center" | "right";
+  text: string;
+  fontSize: number;
+  fontColor: string;
+  backgroundColor: string;
+  placeholder: string;
+  borderRadius: number;
+  height: number; //change to % after
+  width: number; //change to % after
+  alignment: "left" | "center" | "right";
 }
+
+export interface generateTextBoxProps {
+  placeholder: string;
+  height: number;
+  width: number;
+}
+
 export type TooltipConfigText = {
-    label: string;
-    content: string;
-    propKey: keyof TextBoxProps;
-    type: 'color' | 'spinButton' | 'text' | 'alignment';
+  label: string;
+  content: string;
+  propKey: keyof TextBoxProps;
+  type: "color" | "spinButton" | "text" | "alignment";
 };
 
 export interface IconProps {
-    selectedIcon: keyof typeof VscIcons;
-    iconSize?: number;
-    iconColor?: string;
-    hyperlink?: string;
+  selectedIcon: keyof typeof VscIcons;
+  iconSize?: number;
+  iconColor?: string;
+  hyperlink?: string;
 }
 
 export type TooltipConfigIcon = {
-    label: string;
-    content: string;
-    propKey: keyof IconProps;
-    type: 'color' | 'spinButton' | 'text';
+  label: string;
+  content: string;
+  propKey: keyof IconProps;
+  type: "color" | "spinButton" | "text";
 };
 
 export interface ImageProps {
-    src: string;
-    alt: string;
-    width: number;     //change to % after
-    height: number;    //change to % after
-    alignment?: "left" | "center" | "right";
-  }
-  
+  src: string;
+  alt: string;
+  width: number; //change to % after
+  height: number; //change to % after
+  alignment?: "left" | "center" | "right";
+}
+
+export interface generateImageProps {
+  src: string;
+  alt: string;
+  height: number;
+  width: number;
+}
+
 export interface CardContainerProps {
-    color: string;
-    height: number;
-    width: number;
-    flexDirection?: "row" | "column";
-    justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around";
-    alignItems?: "flex-start" | "center" | "flex-end";
-    gap?: number;
-    backgroundColor: string;
-    borderRadius: number;
-    bordercolor: string;
-    padding: number;
-    shadow: boolean;
+  color: string;
+  height: number;
+  width: number;
+  flexDirection?: "row" | "column";
+  justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around";
+  alignItems?: "flex-start" | "center" | "flex-end";
+  gap?: number;
+  backgroundColor: string;
+  borderRadius: number;
+  bordercolor: string;
+  padding: number;
+  shadow: boolean;
 }
 
 export interface TextProps {
-    text: string;
-    fontSize: number;
-    fontColor: string;
-    textAlign: 'left' | 'center' | 'right' | 'justify';
-    bold?: boolean;
-    italic?: boolean;
-    underline?: boolean;
-    hyperlink?: string;
-    placeholder?: string;
+  text: string;
+  fontSize: number;
+  fontColor: string;
+  textAlign: "left" | "center" | "right" | "justify";
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+  hyperlink?: string;
+  placeholder?: string;
+}
+
+export interface generateTextProps {
+  text: string;
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
 }
