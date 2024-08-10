a32bf449b1f88f5b1b92b605e5018055d5e9a0bd
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-08-10 14:15:42 +0100

    input label color

--- src/WinUI3/components/inputTranslator.ts
+++ src/WinUI3/components/inputTranslator.ts
@@ -9,6 +9,8 @@ export function generateInputXaml(node: Node, indent: string = ""): string {
   xaml += ` Foreground="${props.fontColor}"`;
   xaml += ` Background="${props.backgroundColor}"`;
   xaml += ` CornerRadius="${props.borderRadius}"`;
+  xaml += ` BorderBrush="${props.borderColor}"`;
+  xaml += ` BorderThickness="1"`;
 
   xaml += " />";
 
