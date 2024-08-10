e8e1b115b8c0926615672a1c686bd91e3612df87
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-08-10 13:41:50 +0100

    update button components trnaslation

--- src/WinUI3/components/buttonTranslator.ts
+++ src/WinUI3/components/buttonTranslator.ts
@@ -11,36 +11,33 @@ export function generateButtonXaml(node: Node, indent: string = ""): string {
   xaml += ` Foreground="${props.fontColor}"`;
   xaml += ` Background="${props.backgroundColor}"`;
   xaml += ` FontSize="${props.fontSize}"`;
-  xaml += ` Width="${props.width}%"`;
-  xaml += ` Height="${props.height}%"`;
+
+  //Percent not supported in xaml
+  // xaml += ` Width="${props.width}%"`;
+  // xaml += ` Height="${props.height}%"`;
+  xaml += ` HorizontalAlignment="Stretch" VerticalAlignment="Stretch"`;
 
   // Border properties
   xaml += ` BorderBrush="${props.bordercolor}"`;
-  xaml += ` BorderThickness="1"`;
+  xaml += ` BorderThickness="2"`;
   xaml += ` CornerRadius="${props.borderRadius}"`;
 
-  // Shadow effect
+  xaml += ">\n";
+  xaml += `${indent}  <Button.RenderTransform>\n`;
+  xaml += `${indent}    <ScaleTransform ScaleX="${props.width / 100}" ScaleY="${
+    props.height / 100
+  }"/>\n`;
+  xaml += `${indent}  </Button.RenderTransform>\n`;
+
+  if (props.icon !== "none") {
+    xaml += generateButtonContent(props, indent + "  ");
+  }
+
+  xaml += `${indent}</Button>`;
+
+  // Wrap button in a Border for shadow effect if needed
   if (props.shadowColor !== "transparent" && props.shadowBlur > 0) {
-    xaml += ">\n";
-    xaml += `${indent}  <Button.Effect>\n`;
-    xaml += `${indent}    <DropShadowEffect Color="${props.shadowColor}" BlurRadius="${props.shadowBlur}" ShadowDepth="0" OffsetX="${props.shadowOffsetX}" OffsetY="${props.shadowOffsetY}" />\n`;
-    xaml += `${indent}  </Button.Effect>\n`;
-
-    // Add icon if present
-    if (props.icon !== "none") {
-      xaml += generateButtonContent(props, indent + "  ");
-    }
-
-    xaml += `${indent}</Button>`;
-  } else {
-    // If no shadow, close the button tag
-    if (props.icon !== "none") {
-      xaml += ">\n";
-      xaml += generateButtonContent(props, indent + "  ");
-      xaml += `${indent}</Button>`;
-    } else {
-      xaml += " />";
-    }
+    xaml = `${indent}<Border>\n${indent}  <Border.Effect>\n${indent}    <DropShadowEffect Color="${props.shadowColor}" BlurRadius="${props.shadowBlur}" ShadowDepth="0" OffsetX="${props.shadowOffsetX}" OffsetY="${props.shadowOffsetY}" />\n${indent}  </Border.Effect>\n${xaml}\n${indent}</Border>`;
   }
 
   return xaml + "\n";
