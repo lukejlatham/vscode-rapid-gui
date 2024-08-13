67559354bb36b95119f4f79d303d6919443df15f
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-08-13 14:55:52 +0100

    icon

--- src/HTML/components/Icon.ts
+++ src/HTML/components/Icon.ts
@@ -2,18 +2,18 @@ import { Node } from "../JSONParser";
 
 export function generateIconHtml(node: Node): string {
   const props = node.props || {};
-  const iconName = props.selectedIcon || "vsc-primitive-square";
+  const iconName = props.selectedIcon || "add";
   const iconSize = props.iconSize || 24;
-  const iconColor = props.iconColor || "#000000";
+  const iconColor = props.iconColor || "currentColor";
   const hyperlink = props.hyperlink || "";
 
-  const iconSvg = `<svg width="${iconSize}" height="${iconSize}" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="${iconColor}">
-    <use href="#${iconName}" />
-  </svg>`;
+  const iconPath = iconPaths[iconName] || iconPaths.add;
 
   const iconHtml = `
-    <span id="${node.custom.id}" class="icon ${node.custom.id}">
-      ${iconSvg}
+    <span id="${node.custom.id}" class="codicon codicon-${iconName}" style="width: ${iconSize}px; height: ${iconSize}px; color: ${iconColor};">
+      <svg width="${iconSize}" height="${iconSize}" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+        ${iconPath}
+      </svg>
     </span>
   `;
 
@@ -25,36 +25,31 @@ export function generateIconHtml(node: Node): string {
 }
 
 export function generateIconCss(node: Node): string {
-  const props = node.props || {};
-  const iconSize = props.iconSize || 24;
-  const iconColor = props.iconColor || "#000000";
-
   return `
-  .icon.${node.custom.id} {
+  .codicon {
     display: inline-block;
     vertical-align: middle;
-    width: ${iconSize}px;
-    height: ${iconSize}px;
-    color: ${iconColor};
+    line-height: 1;
   }
 
-  .icon.${node.custom.id} svg {
+  .codicon svg {
     width: 100%;
     height: 100%;
   }
-    
+
   .icon-link {
     text-decoration: none;
   }
   `;
 }
 
-// SVG definitions for VSC icons
-export const iconSprite = `
-<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-  <symbol id="vsc-primitive-square" viewBox="0 0 16 16">
-    <path d="M3.5 3.5v9h9v-9h-9zM12 12H4V4h8v8z" />
-  </symbol>
-  <!-- Add other icon symbols here -->
-</svg>
-`;
+export const iconPaths: { [key: string]: string } = {
+  "add": '<path fill-rule="evenodd" clip-rule="evenodd" d="M14 7v1H8v6H7V8H1V7h6V1h1v6h6z"/>',
+  "primitive-square":
+    '<path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 4l.5-.5h8l.5.5v8l-.5.5H4l-.5-.5V4zm1 .5v7h7v-7h-7z"/>',
+  "account":
+    '<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M16 7.992C16 3.58 12.416 0 8 0S0 3.58 0 7.992c0 2.43 1.104 4.62 2.832 6.09.016.016.032.016.032.032.144.112.288.224.448.336.08.048.144.111.224.175A7.98 7.98 0 0 0 8.016 16a7.98 7.98 0 0 0 4.48-1.375c.08-.048.144-.111.224-.16.144-.111.304-.223.448-.335.016-.016.032-.016.032-.032 1.696-1.487 2.8-3.676 2.8-6.106zm-8 7.001c-1.504 0-2.88-.48-4.016-1.279.016-.128.048-.255.08-.383a4.17 4.17 0 0 1 .416-.991c.176-.304.384-.576.64-.816.24-.24.528-.463.816-.639.304-.176.624-.304.976-.4A4.15 4.15 0 0 1 8 10.342a4.185 4.185 0 0 1 2.928 1.166c.368.368.656.8.864 1.295.112.288.192.592.24.911A7.03 7.03 0 0 1 8 14.993zm-2.448-7.4a2.49 2.49 0 0 1-.208-1.024c0-.351.064-.703.208-1.023.144-.32.336-.607.576-.847.24-.24.528-.431.848-.575.32-.144.672-.208 1.024-.208.368 0 .704.064 1.024.208.32.144.608.336.848.575.24.24.432.528.576.847.144.32.208.672.208 1.023 0 .368-.064.704-.208 1.023a2.84 2.84 0 0 1-.576.848 2.84 2.84 0 0 1-.848.575 2.715 2.715 0 0 1-2.064 0 2.84 2.84 0 0 1-.848-.575 2.526 2.526 0 0 1-.56-.848zm7.424 5.306c0-.032-.016-.048-.016-.08a5.22 5.22 0 0 0-.688-1.406 4.883 4.883 0 0 0-1.088-1.135 5.207 5.207 0 0 0-1.04-.608 2.82 2.82 0 0 0 .464-.383 4.2 4.2 0 0 0 .624-.784 3.624 3.624 0 0 0 .528-1.934 3.71 3.71 0 0 0-.288-1.47 3.799 3.799 0 0 0-.816-1.199 3.845 3.845 0 0 0-1.2-.8 3.72 3.72 0 0 0-1.472-.287 3.72 3.72 0 0 0-1.472.288 3.631 3.631 0 0 0-1.2.815 3.84 3.84 0 0 0-.8 1.199 3.71 3.71 0 0 0-.288 1.47c0 .352.048.688.144 1.007.096.336.224.64.4.927.16.288.384.544.624.784.144.144.304.271.48.383a5.12 5.12 0 0 0-1.04.624c-.416.32-.784.703-1.088 1.119a4.999 4.999 0 0 0-.688 1.406c-.016.032-.016.064-.016.08C1.776 11.636.992 9.91.992 7.992.992 4.14 4.144.991 8 .991s7.008 3.149 7.008 7.001a6.96 6.96 0 0 1-2.032 4.907z"/></svg>',
+  "primitive-dot": '<circle cx="8" cy="8" r="3"/>',
+  "primitive-square-filled":
+    '<path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 4l.5-.5h8l.5.5v8l-.5.5H4l-.5-.5V4zm1 .5v7h7v-7h-7z"/>',
+};
