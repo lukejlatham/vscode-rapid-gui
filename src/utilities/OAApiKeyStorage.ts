36451c81e89d0188dc11e6f03570913c3b9e140a
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2024-08-14 11:02:20 +0100

    ADDED: openai api key storage utlilities

---
+++ src/utilities/OAApiKeyStorage.ts
@@ -0,0 +1,30 @@
+import * as vscode from "vscode";
+
+export async function getAzureOpenaiApiKeys(context: vscode.ExtensionContext) {
+  const apiKey = await context.secrets.get("OPENAI_API_KEY");
+  const apiEndpoint = await context.secrets.get("OPENAI_API_ENDPOINT");
+
+  return { apiKey, apiEndpoint };
+}
+
+export async function getOpenaiApiKey(context: vscode.ExtensionContext) {
+  const apiKey = await vscode.window.showInputBox({
+    prompt: "Enter your OpenAI API Key",
+  });
+
+  if (apiKey) {
+    await context.secrets.store("OPEN_API_KEY", apiKey);
+    vscode.window.showInformationMessage("OpenAI API key added to storage.");
+  }
+}
+
+export async function getAzureOpenaiApiEndpoint(context: vscode.ExtensionContext) {
+  const apiEndpoint = await vscode.window.showInputBox({
+    prompt: "Enter your OpenAI API Endpoint",
+  });
+
+  if (apiEndpoint) {
+    await context.secrets.store("OPENAI_API_ENDPOINT", apiEndpoint);
+    vscode.window.showInformationMessage("OpenAI API endpoint added to storage.");
+  }
+}
