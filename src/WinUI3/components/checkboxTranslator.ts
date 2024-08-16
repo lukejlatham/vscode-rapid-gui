d39148cadb8ac9794a8fb27b8f5d786c643ded16
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-08-16 16:59:08 +0200

    checkbox

--- src/WinUI3/components/checkboxTranslator.ts
+++ src/WinUI3/components/checkboxTranslator.ts
@@ -2,16 +2,17 @@ import { Node } from "../JsonParser";
 
 export function generateCheckboxXaml(node: Node, indent: string = ""): string {
   const props = node.props;
-  let xaml = `${indent}<CheckBox`;
+  let xaml = `${indent}<CheckBox Checked="TwoState_Checked" Unchecked="TwoState_Unchecked" `;
 
-  xaml += ` Content="${props.label}"`;
-  xaml += ` IsChecked="${props.checked}"`;
+  xaml += ` Content="${props.label} ?? 'Checkbox'"`;
   xaml += ` FontSize="${props.fontSize}"`;
   xaml += ` Foreground="${props.fontColor}"`;
   xaml += ` Background="${props.backgroundColor}"`;
 
-  if (props.disabled) {
-    xaml += ` IsEnabled="False"`;
+  if (props.direction === "row") {
+    xaml += ` HorizontalAlignment="Left"`;
+  } else {
+    xaml += ` VerticalAlignment="Top"`;
   }
 
   xaml += " />";
