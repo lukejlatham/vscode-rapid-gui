2d7969de8af8e8b122685c8b7ebac8fcadb40d85
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-08-17 20:04:59 +0100

    checkbox

--- src/WinUI3/components/checkboxTranslator.ts
+++ src/WinUI3/components/checkboxTranslator.ts
@@ -2,20 +2,24 @@ import { Node } from "../JsonParser";
 
 export function generateCheckboxXaml(node: Node, indent: string = ""): string {
   const props = node.props;
-  
-  let xaml = `${indent}<StackPanel Orientation="${props.direction === "row" ? "Horizontal" : "Vertical"}">\n`;
 
-  for (let i = 0; i < props.numberOfBoxes; i++) {
+  let xaml = `${indent}<StackPanel Orientation="${
+    props.direction === "row" ? "Horizontal" : "Vertical"
+  }">\n`;
+
+  for (let i = 0; i < (props.numberOfBoxes || 1); i++) {
     const checkboxName = `Option${i + 1}CheckBox`;
 
-    xaml += `${indent}  <CheckBox x:Name="${checkboxName}" Content="${props.optionLabels && props.optionLabels[i] ? props.optionLabels[i] : `Option ${i + 1}`}"`;
+    xaml += `${indent}  <CheckBox x:Name="${checkboxName}" Content="${
+      props.optionLabels && props.optionLabels[i] ? props.optionLabels[i] : `Option ${i + 1}`
+    }"`;
 
-    xaml += ` Margin="24,0,0,0"`;
-    xaml += ` FontSize="${props.fontSize ?? "12"}"`;
-    xaml += ` Foreground="${props.fontColor ?? "Black"}"`;
-    xaml += ` Background="${props.backgroundColor ?? "Transparent"}"`;
-    xaml += ` Checked="Option_Checked"`;
-    xaml += ` Unchecked="Option_Unchecked"`;
+    xaml += ` Margin="0,0,0,8"`;
+    xaml += ` FontSize="${props.fontSize || 14}"`;
+    xaml += ` Foreground="${
+      props.fontColor || "{ThemeResource SystemControlForegroundBaseHighBrush}"
+    }"`;
+    xaml += ` Background="${props.backgroundColor || "Transparent"}"`;
 
     xaml += " />\n";
   }
