44b0ce8e2b9fa0762b7e1412f871fdd0afa4e6b7
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-08-19 20:20:16 +0100

    chore: Refactor settings component to use hooks and add theme selection functionality

--- webview-ui/src/pages/EditingInterface/LeftSidebar/Settings.tsx
+++ webview-ui/src/pages/EditingInterface/LeftSidebar/Settings.tsx
@@ -1,25 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Select, Theme, useId, Label } from '@fluentui/react-components';
 import { teamsDarkTheme, teamsLightTheme, teamsHighContrastTheme } from '@fluentui/react-components';
 import { LanguageContext } from '../../../components/Wrapper';
 import { AccessibilityContext } from '../EditingInterface';
 import { FormattedMessage } from 'react-intl';
+import { set } from 'lodash';
 
 const Settings: React.FC<{
     classes: any;
     theme: Theme;
     setTheme: React.Dispatch<React.SetStateAction<Theme>>;
-}> = ({ classes, setTheme }) => {
+}> = ({ classes, setTheme, theme }) => {
     const language = useContext(LanguageContext);
     const accessibility = useContext(AccessibilityContext);
-
+    const [selectedTheme, setSelectedTheme] = useState<'dark' | 'light' | 'highContrast' >('dark');
     const themeDropdownId = useId('themeDropdown');
     const languageDropdownId = useId('languageDropdown');
     const accessibilityDropdownId = useId('accessibilityDropdown');
 
 
     const handleThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const selected = event.target.value;
+        const selected = event.target.value as 'dark' | 'light' | 'highContrast';
+        setSelectedTheme(selected);
         switch (selected) {
             case 'light':
                 setTheme(teamsLightTheme);
@@ -50,7 +52,7 @@ const Settings: React.FC<{
             <Label htmlFor={themeDropdownId}>
                 <FormattedMessage id="settings.changeTheme" defaultMessage="Change the extension's theme: " />
             </Label>
-            <Select id={themeDropdownId} defaultValue={"dark"} onChange={handleThemeChange}>
+            <Select id={themeDropdownId} value={selectedTheme} onChange={handleThemeChange}>
                 <option value="dark">
                     <FormattedMessage id="settings.dark" defaultMessage="Dark" />
                 </option>
@@ -65,7 +67,7 @@ const Settings: React.FC<{
             <Label htmlFor={languageDropdownId}>
                 <FormattedMessage id="settings.changeLanguage" defaultMessage="Change the extension's language: " />
             </Label>
-            <Select id={languageDropdownId} defaultValue={language.locale} onChange={language.changeLanguage}>
+            <Select id={languageDropdownId} value={language.locale} onChange={language.changeLanguage}>
                 <option value="en">English</option>
                 <option value="fr">French</option>
             </Select>
@@ -73,7 +75,7 @@ const Settings: React.FC<{
             <Label htmlFor={accessibilityDropdownId}>
                 <FormattedMessage id="settings.accessibility" defaultMessage="Enable accessibility features" />
             </Label>
-            <Select id={accessibilityDropdownId} defaultValue={"no"} onChange={handleAccessibilityChange}>
+            <Select id={accessibilityDropdownId} value={accessibility.selectedAccessibility as string} onChange={handleAccessibilityChange}>
                 <option value="no">No</option>
                 <option value="yes">Yes</option>
             </Select>
