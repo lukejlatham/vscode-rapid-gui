12bd781c3e0c3d51a4abeeb5ab039c91a58e52fa
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-08-19 11:18:10 +0100

    fix paragraph

--- src/WinUI3/components/paragraphTranslator.ts
+++ src/WinUI3/components/paragraphTranslator.ts
@@ -1,11 +1,9 @@
-// textblock Paragraph -> <TextBlock TextWrapping="Wrap">
 import { Node } from "../JsonParser";
 
 export function generateTextXaml(node: Node, indent: string = ""): string {
   const props = node.props;
-  let xaml = `${indent}<TextBlock TextWrapping="Wrap"`;
+  let xaml = `${indent}<RichTextBlock TextWrapping="Wrap"`;
 
-  xaml += ` Text="${props.text || ""}"`;
   xaml += ` FontSize="${props.fontSize || 14}"`;
   xaml += ` Foreground="${
     props.fontColor || "{ThemeResource SystemControlForegroundBaseHighBrush}"
@@ -14,25 +12,41 @@ export function generateTextXaml(node: Node, indent: string = ""): string {
     (props.textAlign || "left").charAt(0).toUpperCase() + (props.textAlign || "left").slice(1)
   }"`;
 
-  if (props.bold) {
-    xaml += ` FontWeight="Bold"`;
-  }
-  if (props.italic) {
-    xaml += ` FontStyle="Italic"`;
-  }
-  if (props.underline) {
-    xaml += ` TextDecorations="Underline"`;
+  xaml += ">\n";
+  xaml += `${indent}  <Paragraph>`;
+
+  if (props.hyperlink) {
+    xaml += `<Hyperlink NavigateUri="${props.hyperlink}">`;
   }
 
-  // if (props.hyperlink) {
-  //   xaml += ">\n";
-  //   xaml += `${indent}  <Hyperlink NavigateUri="${props.hyperlink}">\n`;
-  //   xaml += `${indent}    ${props.text}\n`;
-  //   xaml += `${indent}  </Hyperlink>\n`;
-  //   xaml += `${indent}</TextBlock>`;
-  // } else {
-  //   xaml += " />";
-  // }
+  if (props.bold) {
+    xaml += "<Bold>";
+  }
+  if (props.italic) {
+    xaml += "<Italic>";
+  }
+  if (props.underline) {
+    xaml += "<Underline>";
+  }
+
+  xaml += `<Run Text="${props.text || ""}" />`;
+
+  if (props.underline) {
+    xaml += "</Underline>";
+  }
+  if (props.italic) {
+    xaml += "</Italic>";
+  }
+  if (props.bold) {
+    xaml += "</Bold>";
+  }
+
+  if (props.hyperlink) {
+    xaml += "</Hyperlink>";
+  }
+
+  xaml += "</Paragraph>\n";
+  xaml += `${indent}</RichTextBlock>`;
 
   return xaml + "\n";
 }
