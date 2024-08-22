99234dfad311445eeb3a9b879d4e1a024e433502
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-08-22 15:04:48 +0200

    color token translations

---
+++ src/WinUI3/components/colortranslator.ts
@@ -0,0 +1,10 @@
+export const colorTokens: { [key: string]: string } = {
+  colorNeutralStroke1: "#E1E1E1",
+  colorBrandForeground2: "#0078D4",
+  colorNeutralStroke2: "#525252",
+  colorBrandForeground1: "#2899f5",
+  colorNeutralBackground3: "#141414",
+  // got some from our other pages but need to add more
+};
+
+//must add tokens also for the borderRadiusMedium so border size?
